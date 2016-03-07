64de554af8d6d01a493390dc43b2d79a763b41bb
Benedict Lau <ben.hy.lau@gmail.com>
2016-03-06 22:49:22 -0500

    Change back to gsed

--- license_swap.sh
+++ license_swap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-LICENSE_TEXT=`cat license_text.txt | sed -z 's/\n/##NEWLINE_CHAR##/g'` && sed -z 's/\/\*.*\*\/\npackage/##LICENSE_HEADER##\npackage/' $1 | awk -v licenseText="$LICENSE_TEXT" '{ gsub("##LICENSE_HEADER##", licenseText); gsub("##NEWLINE_CHAR##", "\n"); print }' > $1.tmp && mv $1.tmp $1
+LICENSE_TEXT=`cat license_text.txt | gsed -z 's/\n/##NEWLINE_CHAR##/g'` && gsed -z 's/\/\*.*\*\/\npackage/##LICENSE_HEADER##\npackage/' $1 | awk -v licenseText="$LICENSE_TEXT" '{ gsub("##LICENSE_HEADER##", licenseText); gsub("##NEWLINE_CHAR##", "\n"); print }' > $1.tmp && mv $1.tmp $1
